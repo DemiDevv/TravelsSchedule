8117312f3ef3ae63258b971e80ca0db5d7285032
Demian Petropavlov <demyanpain@gmail.com>
2025-03-09 21:26:24 +0400

    Проект создан

---
+++ TravelsSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelsSchedule
+//
+//  Created by Demain Petropavlov on 09.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
